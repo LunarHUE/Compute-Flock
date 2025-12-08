918868d8a0413172cf41d6775a68169ba04976c4
Wesley Baldwin <74224353+Legonois@users.noreply.github.com>
2025-12-08 20:54:59 +0000

    dsawd

--- pkg/discovery/controller.go
+++ pkg/discovery/controller.go
@@ -18,33 +18,28 @@ func RunControllerMode(NodeID string, Port uint16, callback func(ip string, role
 
 	// 2. Define the Callback for finding new nodes
 	onNodeFound := func(e zeroconf.Event) {
-		// Only react to "Added" events with valid IPs
 		if e.Op == zeroconf.OpAdded && len(e.Addrs) > 0 {
 			meta := parseMetadata(e.Text)
 
 			log.Infof("------------------------------------------------")
-			log.Infof("   CANDIDATE FOUND: %s\n", e.Name)
-			log.Infof("   IP:   %s\n", e.Addrs[0])
-			log.Infof("   OS:   %s (%s)\n", meta["os"], meta["distro"])
-			log.Infof("   HW:   %s Cores / %s GB RAM / %s GB Disk\n", meta["cpu"], meta["mem"], meta["disk"])
-			log.Infof("   MAC:  %s\n", meta["mac"])
+			log.Infof("   CANDIDATE FOUND: %s", e.Name)
+			log.Infof("   IP:   %s", e.Addrs[0])
+			log.Infof("   OS:   %s (%s)", meta["os"], meta["distro"])
+			log.Infof("   HW:   %s Threads / %s GB RAM / %s GB Disk", meta["cpu"], meta["mem"], meta["disk"])
+			log.Infof("   MAC:  %s", meta["mac"])
 			log.Infof("------------------------------------------------")
 
 			log.Infof("Found new node: %s [%v]. Auto-adopting...", e.Name, e.Addrs)
 
-			// Extract IP (prefer IPv4)
 			ip := e.Addrs[0].String()
-			// Note: You might want to iterate e.Addrs to find the IPv4 one specifically
-			// if the network has both.
 
 			go callback(ip, "agent")
 		}
 	}
 
-	// 3. Start the Engine (Publish Myself + Browse for Others)
 	client, err := zeroconf.New().
-		Publish(me).                      // "I am the Controller"
-		Browse(onNodeFound, TypePending). // "Look for Pending Nodes"
+		Publish(me).
+		Browse(onNodeFound, TypePending).
 		Open()
 
 	if err != nil {
